5c8f57b201531bba7ae000f3bc075d8116c625ac
dhythm <yuta.okada.20@gmail.com>
2024-09-01 15:37:14 +0900

    add static settings for using gunicorn

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
