4ab2832e29bed7a39de3a43d8ac7c1350b3539ac
dhythm <yuta.okada.20@gmail.com>
2024-09-01 10:13:42 +0900

    add filter and search to QuestionAdmin, add ChoiceAdmin with autocomplete_fields

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -15,7 +15,13 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
     list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    autocomplete_fields = ["question"]
 
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+admin.site.register(Choice, ChoiceAdmin)
