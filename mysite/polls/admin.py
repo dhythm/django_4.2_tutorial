465176896492dc3c893d1c650cdb7c22f7a2256b
dhythm <yuta.okada.20@gmail.com>
2024-09-01 10:03:08 +0900

    customize polls admin with ChoiceInline

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,8 +3,17 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["pub_date", "question_text"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
